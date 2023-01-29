15aa1396058b9f176dc1214a7716a0afdaaf5a46
hjubs <hamma.juba@gmail.com>
2023-01-30 00:47:48 +0100

    Reset db and printing some stuff with py

--- plotting_data/plot_figs_belib.py
+++ plotting_data/plot_figs_belib.py
@@ -6,4 +6,17 @@
 # ===========================================================================
 
 import matplotlib as mpl
+import sqlite3 as sql3
+
+path_to_db = "../traitement_data/sqlitedb/"
+db_filename = "records_belib.db"
+
+con = sql3.connect(path_to_db+db_filename)
+cur = con.cursor()
+cur.execute("SELECT * FROM Records")
+names = list(map(lambda x: x[0], cur.description))
+result = cur.fetchall()
+
+print(names)
+print(*result, sep="\n")
 
