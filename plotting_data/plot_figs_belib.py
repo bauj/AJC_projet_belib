08a676ec4cdef84ae959eefab116e679583b2401
hjubs <hamma.juba@gmail.com>
2023-01-29 20:36:20 +0100

    Debut du plotting ...

---
+++ plotting_data/plot_figs_belib.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# ===========================================================================
+# Script de plotting de figures illustrant les donnees de la db sqlite belib.
+# Author : Juba Hamma.
+# ===========================================================================
+
+import matplotlib as mpl
+
