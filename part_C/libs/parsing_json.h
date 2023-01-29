e56060b8c819c67fbaa208472b7b93f9c2555c01
hjubs <hamma.juba@gmail.com>
2023-01-29 16:52:31 +0100

    Un peu de nettoyage ...

--- part_C/libs/parsing_json.h
+++ part_C/libs/parsing_json.h
@@ -17,19 +17,14 @@ typedef struct record {
     char *statut_pdc; /**< Statut des bornes*/
 } record;
 
-// ----------------------------------------------------------------------------
+
 /**
  * @brief Fonction d'affichage du contenu d'un record
  * 
  * @param rec Record à afficher
  */
-void print_record(record rec)
-{
-    printf("Statut           : %s \n", rec.statut_pdc);
-    printf("Nombre de bornes : %d \n", rec.nb_bornes);
-}
+void print_record(record rec);
 
-// ----------------------------------------------------------------------------
 /**
  * @brief Fonction permettant de slicer une chaine de caractères
  * 
@@ -38,12 +33,9 @@ void print_record(record rec)
  * @param start Indice de départ pour le slicing
  * @param end Indice de fin pour le slicing
  */
-void slice(const char *str_src, char *str_dest, int start, int end)
-{
-    strncpy(str_dest, str_src + start, end - start);
-}
+void slice(const char *str_src, char *str_dest, int start, int end);
 
-// ----------------------------------------------------------------------------
+
 /**
  * @brief Recuperation de la date de collecte a partir du nom du fichier
  * Un nom de fichier finit par la date du jour de collecte :
@@ -53,13 +45,8 @@ void slice(const char *str_src, char *str_dest, int start, int end)
  * @param date_recolte Chaine de caractères contenant la date
  * @return void* 
  */
-void recup_date(const char *json_filename, char* date_recolte) {
-    // On recupere la fin du nom de fichier. Permet d'avoir un basename différent.
-    char *last_piece = strrchr(json_filename, '_');
-    slice(last_piece, date_recolte, 1, 11); /**< le slice n'ajoute pas de \0 en fin de string*/
-}
+void recup_date(const char *json_filename, char* date_recolte);
 
-// ----------------------------------------------------------------------------
 /**
  * @brief Lecture du contenu du fichier json passé en argument.
  * 
@@ -67,7 +54,66 @@ void recup_date(const char *json_filename, char* date_recolte) {
  * @param json_filename Nom du fichier json a lire.
  * @return char* Renvoie la chaine de caracteres contenue dans le fichier.
  */
-char *lecture_contenu_json(const char *json_filename) {
+char *lecture_contenu_json(const char *json_filename);
+
+/**
+ * @brief Verifie que le contenu de la chaine parsée et du token recupéré est le même.
+ * Ce qui est vérifié : Type du token, longueur du token, et contenu du token
+ * 
+ * @param json Chaine de caractère parsée
+ * @param tok Token en cours d'analyse
+ * @param s Chaine de caractère à lire
+ * @return int 0 si OK, -1 si KO
+ */
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s);
+
+
+/**
+ * @brief Affiche le contenu d'un token (string)
+ * 
+ * @param json Contenu du fichier json
+ * @param tok Token a afficher
+ */
+void print_token(const char *json, jsmntok_t *tok);
+
+
+/**
+ * @brief Recupere le contenu (string) d'un token.
+ * 
+ * @warning La fonction slice est utilisée, on verifie donc que la chaine finit bien par '\0'.
+ * @warning Le pointeur tok_str est alloué dynamiquement et doit donc etre désalloué.
+ * @param json Contenu du fichier json
+ * @param tok Token dont le contenu est récupéré
+ * @return char* String contenue dans le token
+ */
+char *get_token_string(const char *json, jsmntok_t *tok);
+
+
+
+// ----------------------------------------------------------------------------
+void print_record(record rec)
+{
+    printf("Statut           : %s \n", rec.statut_pdc);
+    printf("Nombre de bornes : %d \n", rec.nb_bornes);
+}
+
+// ----------------------------------------------------------------------------
+void slice(const char *str_src, char *str_dest, int start, int end)
+{
+    strncpy(str_dest, str_src + start, end - start);
+}
+
+// ----------------------------------------------------------------------------
+void recup_date(const char *json_filename, char* date_recolte) 
+{
+    // On recupere la fin du nom de fichier. Permet d'avoir un basename différent.
+    char *last_piece = strrchr(json_filename, '_');
+    slice(last_piece, date_recolte, 1, 11); /**< le slice n'ajoute pas de \0 en fin de string*/
+}
+
+// ----------------------------------------------------------------------------
+char *lecture_contenu_json(const char *json_filename) 
+{
     // Ouverture du fichier json
     FILE *json_file = fopen(json_filename, "r");
     
@@ -106,16 +152,8 @@ char *lecture_contenu_json(const char *json_filename) {
 }
 
 // ----------------------------------------------------------------------------
-/**
- * @brief Verifie que le contenu de la chaine parsée et du token recupéré est le même.
- * Ce qui est vérifié : Type du token, longueur du token, et contenu du token
- * 
- * @param json Chaine de caractère parsée
- * @param tok Token en cours d'analyse
- * @param s Chaine de caractère à lire
- * @return int 0 si OK, -1 si KO
- */
-static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s)
+{
   if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
       strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
     return 0;
@@ -123,26 +161,15 @@ static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
   return -1;
 }
 
-/**
- * @brief Affiche le contenu d'un token (string)
- * 
- * @param json Contenu du fichier json
- * @param tok Token a afficher
- */
-void print_token(const char *json, jsmntok_t *tok) {
+// ----------------------------------------------------------------------------
+void print_token(const char *json, jsmntok_t *tok) 
+{
     printf("%.*s \n", tok->end - tok->start, json + tok->start);
 }
 
-/**
- * @brief Recupere le contenu (string) d'un token.
- * 
- * @warning La fonction slice est utilisée, on verifie donc que la chaine finit bien par '\0'.
- * @warning Le pointeur tok_str est alloué dynamiquement et doit donc etre désalloué.
- * @param json Contenu du fichier json
- * @param tok Token dont le contenu est récupéré
- * @return char* String contenue dans le token
- */
-char *get_token_string(const char *json, jsmntok_t *tok) {
+// ----------------------------------------------------------------------------
+char *get_token_string(const char *json, jsmntok_t *tok) 
+{
     int len_str = tok->end - tok->start;
     // Allocation de la chaine renvoyée
     char *tok_str = (char *)malloc((len_str+1) * sizeof(char));
